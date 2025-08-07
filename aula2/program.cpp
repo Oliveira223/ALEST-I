504a1ae13cc6d6ecc99ca752baaa52bc49da55e6
Pedro Oliveira <162735266+Oliveira223@users.noreply.github.com>
2025-08-07 19:14:09 +0000

    aula2, func5 e func6

--- aula2/program.cpp
+++ aula2/program.cpp
@@ -9,12 +9,15 @@ class function{
     int f2(int n);
     int f3(int n);
     int f4(int n);
-    // int f5(int n);
+    int f5(int n);
+    int f6(int n);
 };
 
 
 
 int main(){
+
+    //time  ./progrma   #executa mostrando o tempo
 
     function f;
     ofstream arquivo("saida.csv");
@@ -40,6 +43,21 @@ int main(){
     //F4
     for(int n = 1; n < 1000; n++){
         arquivo << f.f4(n) << "; ";
+    }
+    arquivo << endl;
+
+    //F5
+    for(int n = 1; n < 1000; n++){
+        arquivo << f.f5(n) << "; ";
+    }
+    arquivo << endl;
+
+    //F6
+        //30  -> 4 
+        //32  -> 18s
+        //+32 -> ?
+    for(int n = 1; n < 10; n++){
+        arquivo << f.f6(n) << "; ";
     }
     arquivo << endl;
 
@@ -85,15 +103,7 @@ int function::f4(int n){
     for(int i = 1; i < n; i++){
         for(int j = i; j < 2*i; j++){
            for(int k = i; k < j; k++){
-               r += 1;
-
-            //    cout << "R = " << r << endl;
-            //    cout << "i = " << i << endl;
-            //    cout << "j = " << j << endl;
-            //    cout << "k = " << k << endl;
-               
-               
-               
+               r += 1;    
            }
         }
     }
@@ -101,3 +111,23 @@ int function::f4(int n){
 }
 
 
+int function::f5(int n){
+    int r = 0;
+    for(int i = 1; i < n; i++){
+        for(int j = i; j < i + 3; j++){
+            for(int k = i; k < j; k++){
+            r += 1;
+            }
+        }
+    }
+    return r;
+}
+
+int function::f6(int n){
+    if(n == 0) {
+        return 1;
+    }
+    else 
+        return f6(n-1) + f6(n - 1);
+     
+}
