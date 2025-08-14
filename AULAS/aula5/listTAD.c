0e706bec0c8c14ce99a10bbdd4660668223a8b98
Pedro Oliveira <162735266+Oliveira223@users.noreply.github.com>
2025-08-14 14:15:33 +0000

    aula 5 lista encadeada

---
+++ AULAS/aula5/listTAD.c
@@ -0,0 +1,19 @@
+// ******************************************
+//  Classe abstrata ListTAD (INTERFACE)
+// ******************************************
+
+class ListTAD {
+    public:
+        virtual void add(int element) = 0;
+        virtual void add(int element, int pos) = 0;
+        virtual int get(int index) = 0;
+        virtual int set(int index, int element) = 0;
+        virtual bool remove(int element) = 0;
+        virtual bool isEmpty() = 0;
+        virtual int size() = 0;
+        virtual int capacity() = 0;
+        virtual bool contains(int element) = 0;
+        virtual int indexOf(int element) = 0;
+        virtual void clear() = 0;
+};
+
