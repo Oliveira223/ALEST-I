5ad5dc632e5f87ca35086f96a7eb57964c572945
Pedro Oliveira <162735266+Oliveira223@users.noreply.github.com>
2025-09-25 18:41:24 +0000

    funções basicas concluidas

--- AULAS/aula7_double_linked/app.cpp
+++ AULAS/aula7_double_linked/app.cpp
@@ -18,66 +18,40 @@ int main()
 
     lista->print();
 
+    lista->add(0, 1);
 
-    // lista.add(0, 1);
+    cout << "Posição do elemento 8:" << lista->indexOf(8) << endl;
+    cout << "Posição do elemento 22:" << lista->indexOf(22) << endl;
+    cout << "Existe o elemento 22? " << lista->contains(22) << endl;
+    cout << "Elemento armazenado na primeira posicao da lista: " << lista->get(0) << endl;
+    cout << "Elemento armazenado na ultima posicao da lista: " << lista->get(lista->size() - 1) << endl;
 
-    // cout << lista.toString() << endl;
 
-    // cout << "Posição do elemento 8:" << lista.indexOf(8) << endl;
-    // cout << "Posição do elemento 22:" << lista.indexOf(22) << endl;
-    // cout << "Existe o elemento 22? " << lista.contains(22) << endl;
+    cout << "\nAlterando o terceiro elemento para 30" << endl;
+    lista->set(2, 30);
+    lista->print();
 
-    // cout << "Elemento armazenado na primeira posicao da lista: " << lista.get(0) << endl;
-    // cout << "Elemento armazenado na ultima posicao da lista: " << lista.get(lista.size() - 1) << endl;
-    // cout << "Posição do 8: " << lista.indexOf(8) << endl;
+    cout << "\nInserindo o 7 na quinta posição" << endl;
+    lista->add(4, 7);
+    lista->print();
 
-    // cout << "\nAlterando o terceiro elemento para 30" << endl;
-    // lista.set(2, 30);
+    cout << "\nInserindo o 11 na última posição" << endl;
+    lista->add(lista->size() - 1, 11);
+    lista->print();
 
-    // cout << lista.toString() << endl;
+    cout << "\nRemovendo o elemento 30" << endl;
+    lista->remove(30);
+    lista->print();
 
-    // cout << "\nInserindo o 7 na quinta posição" << endl;
-    // lista.add(4, 7);
-    // cout << lista.toString() << endl;
+    cout << "\nRemovendo o elemento na posição 3" << endl;
+    lista->removeByIndex(3);
+    lista->print();
 
-    // cout << "\nInserindo o 11 na última posição" << endl;
-    // lista.add(lista.size() - 1, 11);
-    // cout << lista.toString() << endl;
-
-    // int elem = lista.removeByIndex(0);
-    // cout << endl;
-    // cout << "Removendo o primeiro" << endl;
-    // cout << "removido: " << elem << endl;
-    // cout << lista.toString() << endl;
-
-    // cout << endl;
-    // elem = lista.removeByIndex(4);
-    // cout << "Removendo o quinto elemento" << endl;
-    // cout << "removido: " << elem << endl;
-    // cout << lista.toString() << endl;
-
-    // cout << endl;
-    // elem = lista.removeByIndex(lista.size() - 1);
-    // cout << "Removendo o último elemento" << endl;
-    // cout << "removido: " << elem << endl;
-    // cout << lista.toString() << endl;
-
-    // Descomente para apagar a lista inteira
-    /*
-    while (!lista.isEmpty())
-    {
-        lista.removeByIndex(0);
-        cout << lista.toString() << endl;
-    }
-    */
-
-    /*
-    bool ok = lista.remove(6);
-    cout << endl;
-    if (ok)
-        cout << "OK! Consegui remover!" << endl;
-    else
-        cout << "Elemento não existe" << endl;
-    cout << lista.toString();
-    */
+    cout << "\nTamanho da lista: " << lista->size() << endl;
+    cout << "A lista está vazia? " << (lista->isEmpty() ? "Sim" : "Não") << endl;
+    cout << "\nLimpando a lista..." << endl;
+    lista->clear();
+    lista->print();
+    
+     return 0;
 }
