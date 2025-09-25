c491590ab92d5b2150ea4d70ebe3ace74a749e91
Pedro Oliveira <162735266+Oliveira223@users.noreply.github.com>
2025-09-25 14:09:10 +0000

    patch

--- AULAS/aula7_double_linked/ListDoubleLinked.h
+++ AULAS/aula7_double_linked/ListDoubleLinked.h
@@ -8,6 +8,25 @@
 // Lista Duplamente Encadeada
 // =============================
 
+        // virtual void print() = 0;
+        // virtual void add(int element) = 0;
+        // virtual void add(int index, int element) = 0;
+        // virtual int  get(int index) = 0;
+        // virtual int  set(int index, int element) = 0;
+        // virtual bool remove(int element) = 0;
+        // virtual int  removeByIndex(int index) = 0;
+        // virtual bool isEmpty() = 0;
+        // virtual int  size() = 0;
+        // virtual bool contains(int element) = 0;
+        // virtual int  indexOf(int element) = 0;
+        // virtual void clear() = 0;
+        // virtual void addFirst(int e) = 0;
+        // virtual int  getFirst() = 0;
+        // virtual int  getLast() = 0;
+        // virtual int  removeFirst() = 0;
+        // virtual int  removeLast() = 0;
+
+
 class ListDoubleLinked : public ListTAD { 
 
     // Referência para o sentinela de início
@@ -29,9 +48,15 @@ public:
     bool isEmpty();                     //done
     int  size();                        //done
     bool contains(int element);         //done
-    int  indexOf(int element);          //done
+    int  indexOf(int element);          //done (testar)
     void clear();                       //done
     std::string toString();             //done
+    void addFirst(int e);               
+    int  getFirst();                    
+    int  getLast();
+    int  removeFirst();
+    int  removeLast();
+    
 };
 
 #endif
