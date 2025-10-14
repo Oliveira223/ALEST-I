82bcf06d482d4de9d458dc2cba0caf7d61d8a92e
Pedro Oliveira <162735266+Oliveira223@users.noreply.github.com>
2025-10-14 14:09:21 +0000

    inicio arvores

--- AULAS/aula13_arvores/TreeTAD.cpp
+++ AULAS/aula13_arvores/TreeTAD.cpp
@@ -1,8 +1,18 @@
 #include <iostream> 
 #include <vector>
-#incclude "TreeTAD.h"
+#include "TreeTAD.h"
 
 using namespace std;
+
+// virtual void addChild(TreeTAD* tree) = 0;
+// virtual void addChild(int index, TreeTAD tree) = 0;
+// virtual Tree removeChild(int index) = 0;
+// virtual Tree getChild(int index) = 0;
+// virtual int  childrenCount() = 0;
+// virtual int  size() = 0;        
+// virtual int  getItem() = 0;
+// virtual TreeTAD* getParent() = 0;
+
 
 class Tree : public TreeTAD{
     private: 
@@ -18,13 +28,41 @@ class Tree : public TreeTAD{
         }
         ~Tree(){
             for( TreeTAD* child : children){
-                delete child
+                delete child;
             }
         }
 
         void addChild(TreeTAD* tree){
-            children.push_back(tree)
+            children.push_back(tree);
             Tree* tree2 = dynamic_cast<Tree*>(tree);
             tree2 -> parent = this;
         }
-    }
+
+        void addChild(int index, TreeTAD* Tree){
+            if (index >= 0 && index < children.size()){
+            
+            
+            }
+        }
+
+
+        // Remove um nó filho na posição especificada
+        TreeTAD* removeChild(int index){
+            if (index >= 0 && index < children.size()){
+                Tree* removeChild = dynamic_cast<Tree*>(children(index));
+                child.erase(children.begin() + index);
+                removeChild -> parent = nullptr; // Remove o pai do nó
+                return removeChild;
+            }
+            return nullptr;
+        }
+    };
+
+
+
+int main()
+{
+    Tree* tree = new Tree(10);
+
+    return 0;
+}
